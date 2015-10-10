64fae1bd2205a71effacae4e69c2f9c3546832a1
miciek <miciek@gmail.com>
2015-10-10 13:22:33 +0200

    Refactoring of the RestInterface: changed routes

--- src/main/scala/com/michalplachta/shoesorter/api/RestInterface.scala
+++ src/main/scala/com/michalplachta/shoesorter/api/RestInterface.scala
@@ -4,9 +4,11 @@ import akka.actor._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Flow
 import akka.util.Timeout
 import com.michalplachta.shoesorter.Domain.{Container, Junction}
 import com.michalplachta.shoesorter.Messages.{Go, WhereShouldIGo}
@@ -20,13 +22,13 @@ object RestInterface {
     implicit val timeout = Timeout(5 seconds)
 
     val route =
-      get {
-        path("decisions" / IntNumber / IntNumber) { (junctionId, containerId) =>
+      path("junctions" / IntNumber / "decisionForContainer" / IntNumber) { (junctionId, containerId) =>
+        get {
           complete {
             decider
               .ask(WhereShouldIGo(
-              Junction(junctionId),
-              Container(containerId)))
+                    Junction(junctionId),
+                    Container(containerId)))
               .mapTo[Go]
           }
         }
