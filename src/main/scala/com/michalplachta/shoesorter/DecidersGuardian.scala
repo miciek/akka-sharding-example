e25a349d140febee54ebb10dc91efd9352d9039d
miciek <miciek@gmail.com>
2015-10-13 10:54:16 +0200

    Introduce DecidersGuardian that creates worker for each junction

--- src/main/scala/com/michalplachta/shoesorter/DecidersGuardian.scala
+++ src/main/scala/com/michalplachta/shoesorter/DecidersGuardian.scala
@@ -7,3 +7,14 @@ import com.michalplachta.shoesorter.Messages._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
+class DecidersGuardian extends Actor {
+  def receive = {
+    case msg: WhereShouldIGo =>
+      val name = s"J${msg.junction.id}"
+      val worker = context.child(name) getOrElse createChild(name)
+      worker forward msg
+  }
+
+  private def createChild(name: String) =
+    context.actorOf(Props(new SortingDecider), name)
+}
--- src/main/scala/com/michalplachta/shoesorter/api/SingleNodeApp.scala
+++ src/main/scala/com/michalplachta/shoesorter/api/SingleNodeApp.scala
@@ -1,12 +1,12 @@
 package com.michalplachta.shoesorter.api
 
 import akka.actor.{ActorSystem, Props}
-import com.michalplachta.shoesorter.SortingDecider
+import com.michalplachta.shoesorter.{DecidersGuardian, SortingDecider}
 import com.typesafe.config.ConfigFactory
 
 object SingleNodeApp extends App {
   implicit val system = ActorSystem("sorter")
 
-  val decider = system.actorOf(Props[SortingDecider])
+  val decider = system.actorOf(Props[DecidersGuardian])
   system.actorOf(Props(new RestInterface(decider, 8080)))
 }
