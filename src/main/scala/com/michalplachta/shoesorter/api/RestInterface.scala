b7ca1df5d3a0f2b72b6e19469e20c3a22a8e3303
miciek <miciek@gmail.com>
2015-10-13 03:10:26 +0200

    RestInterface refactorings

--- src/main/scala/com/michalplachta/shoesorter/api/RestInterface.scala
+++ src/main/scala/com/michalplachta/shoesorter/api/RestInterface.scala
@@ -14,26 +14,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object RestInterface {
-  def props(decider: ActorRef, port: Int) = Props(new RestInterface(decider, port))
+  def props(decider: ActorRef, exposedPort: Int) = Props(new RestInterface(decider, exposedPort))
 }
 
 class RestInterface(decider: ActorRef, exposedPort: Int) extends Actor with ActorLogging with HttpServiceBase {
-  implicit val system = context.system
-  implicit val timeout = Timeout(5 seconds)
-
-  val route: Route =
+  val route: Route = {
     path("junctions" / IntNumber / "decisionForContainer" / IntNumber) { (junctionId, containerId) =>
       get {
         complete {
-          log.info(s"Request for $junctionId")
-          decider.ask(
-            WhereShouldIGo(Junction(junctionId), Container(containerId))
-          ).mapTo[Go]
+          log.info(s"Got request on junction $junctionId for container $containerId")
+
+          implicit val timeout = Timeout(5 seconds)
+          val junction = Junction(junctionId)
+          val container = Container(containerId)
+          val futureDecision = decider.ask(WhereShouldIGo(junction, container))
+          futureDecision.mapTo[Go]
         }
       }
     }
+  }
 
   def receive = runRoute(route)
 
+  implicit val system = context.system
   IO(Http) ! Http.Bind(self, interface = "0.0.0.0", port = exposedPort)
 }
