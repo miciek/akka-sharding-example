a98f6d0a8a5a2025319a39007ba8360b93b8f356
miciek <miciek@gmail.com>
2015-11-29 21:16:42 +0100

    step0 contains all messages and domain objects definitions

--- src/main/scala/com/michalplachta/shoesorter/Messages.scala
+++ src/main/scala/com/michalplachta/shoesorter/Messages.scala
@@ -4,4 +4,9 @@ import com.michalplachta.shoesorter.Domain.{Container, Junction}
 import spray.json.DefaultJsonProtocol._
 
 object Messages {
+  case class Go(targetConveyor: String)
+
+  object Go {
+    implicit val goJson = jsonFormat1(Go.apply)
+  }
 }
